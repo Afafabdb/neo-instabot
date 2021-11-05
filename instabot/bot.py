3fecc6ea1c8d1647959e1467183321477887a4d6
Afafabdb <91419650+Afafabdb@users.noreply.github.com>
2021-11-05 14:56:14 +0530

    Update bot.py

--- instabot/bot.py
+++ instabot/bot.py
@@ -156,7 +156,7 @@ class InstaBot:
 
         self.proxies = self.config.get('proxies')
         if self.proxies:
-            self.logger.debug(f"using proxy configuration {self.proxies}")
+            self.logger.debug("using proxy configuration {self.proxies}")
             self.s.proxies.update(self.proxies)
             self.c.proxies.update(self.proxies)
 
